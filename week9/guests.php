f2721589a7c197211f433020412e6f839530b757
aalsuarez <119918293+aalsuarez@users.noreply.github.com>
2023-02-11 00:05:45 +0800

    fixagain

--- week9/guests.php
+++ week9/guests.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM alsuarez_myguests";
+$sql = "SELECT id, name, email FROM alsuarez_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " Email: " . $row["email"]. "<br>";
   }
 } else {
   echo "0 results";
