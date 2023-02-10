dfa5cca71026f1cbca6f42e5bef8c9cc8af879cf
aalsuarez <119918293+aalsuarez@users.noreply.github.com>
2023-02-10 23:59:18 +0800

    fix3

--- week9/guests.php
+++ week9/guests.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]."email: ".$email["email"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]." email: ".$row["email"]. "<br>";
   }
 } else {
   echo "0 results";
